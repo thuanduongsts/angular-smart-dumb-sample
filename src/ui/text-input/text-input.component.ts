d67045f3ba48658bdbe5cf4dce32bfb0d29cb1eb
thuanduongsts <127167693+thuanduongsts@users.noreply.github.com>
2025-05-09 22:29:08 +0700

    feat: input text component (#2)

---
+++ src/ui/text-input/text-input.component.ts
@@ -0,0 +1,18 @@
+import { booleanAttribute, Component, forwardRef, input } from '@angular/core';
+import { FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { BaseControlAccessorComponent } from '../base-control-accessor.directive';
+
+@Component({
+  selector: 'app-text-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './text-input.component.html',
+  styleUrl: './text-input.component.sass',
+  providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: TextInputComponent, multi: true }]
+})
+export class TextInputComponent extends BaseControlAccessorComponent<string> {
+  public readonly placeholder = input<string>('');
+  public readonly isShowClear = input<boolean, StrOrBool>(false, { transform: booleanAttribute });
+  public readonly isRequired = input<boolean, StrOrBool>(false, { transform: booleanAttribute });
+}
